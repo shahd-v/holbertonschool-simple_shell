123ca284fb990007e3717f822e0c0969a0117c60
leen <leenalgraawi@gmail.com>
2026-05-20 19:14:21 +0300

    fixing headers

--- shell.h
+++ shell.h
@@ -13,23 +13,19 @@
 extern char **environ;
 extern int last_status;
 
-/* main.c */
-int shell_loop(char *prog_name);
+/* main.c  static methods */
 
 /* tokenize.c */
-char **tokenize(char *line);
-void free_tokens(char **tokens);
+char *read_line(void);
+void trim_newline(char *line);
+int is_empty(const char *line);
+char **split_line(char *line);
 
 /* path.c */
-char *find_in_path(char *command);
-char *get_env_value(const char *name);
-
-/* execute.c */
-int execute_command(char **args, char *prog_name, int line_num);
+char *find_path(char *cmd);
 
 /* builtins.c */
-int handle_builtin(char **args, char *line);
-void print_env(void);
+int print_env(void);
 int is_number(char *s);
 
 #endif /* SHELL_H */
