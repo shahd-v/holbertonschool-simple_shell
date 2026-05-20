050db47bbbe48a528521e6d0c525b82238d46305
leen <leenalgraawi@gmail.com>
2026-05-20 16:55:38 +0300

    fixing errors

--- execute.c
+++ execute.c
@@ -9,6 +9,7 @@
 static void print_not_found(char *prog_name, int line_num, char *cmd)
 {
 	fprintf(stderr, "%s: %d: %s: not found\n", prog_name, line_num, cmd);
+	exit(127);
 }
 
 /**
--- shell.h
+++ shell.h
@@ -1,6 +1,5 @@
 #ifndef SHELL_H
 #define SHELL_H
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
