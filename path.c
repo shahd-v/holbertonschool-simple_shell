2aba854097a0e500dcbe1f3584fc8850c1f0124d
leen <165122715+oceex@users.noreply.github.com>
2026-05-23 18:43:06 +0300

    Refactor find_path function for clarity and efficiency

--- path.c
+++ path.c
@@ -15,43 +15,43 @@
  */
 char *find_path(char *cmd)
 {
-char *copy, *dir, *full;
-int i;
-if (strchr(cmd, '/'))
-{
-if (access(cmd, X_OK) == 0)
-return (strdup(cmd));
-return (NULL);
-}
-copy = NULL;
-for (i = 0; environ[i]; i++)
-{
-if (strncmp(environ[i], "PATH=", 5) == 0)
-{
-copy = strdup(environ[i] + 5);
-break;
-}
-}
-if (!copy)
-return (NULL);
-dir = strtok(copy, ":");
-while (dir)
-{
-full = malloc(strlen(dir) + strlen(cmd) + 2);
-if (!full)
-{
-free(copy);
-return (NULL);
-}
-sprintf(full, "%s/%s", dir, cmd);
-if (access(full, X_OK) == 0)
-{
-free(copy);
-return (full);
-}
-free(full);
-dir = strtok(NULL, ":");
-}
-free(copy);
-return (NULL);
+  char *copy, *dir, *full;
+  int i;
+  if (strchr(cmd, '/'))
+  {
+    if (access(cmd, X_OK) == 0)
+      return (strdup(cmd));
+    return (NULL);
+  }
+  copy = NULL;
+  for (i = 0; environ[i]; i++)
+  {
+    if (strncmp(environ[i], "PATH=", 5) == 0)
+    {
+      copy = strdup(environ[i] + 5);
+      break;
+    }
+  }
+  if (!copy)
+    return (NULL);
+  dir = strtok(copy, ":");
+  while (dir)
+  {
+    full = malloc(strlen(dir) + strlen(cmd) + 2);
+    if (!full)
+    {
+      free(copy);
+      return (NULL);
+    }
+    sprintf(full, "%s/%s", dir, cmd);
+    if (access(full, X_OK) == 0)
+    {
+      free(copy);
+			return (full);
+    }
+    free(full);
+    dir = strtok(NULL, ":");
+  }
+  free(copy);
+  return (NULL);
 }
